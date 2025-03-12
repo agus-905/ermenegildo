ef565876107fa1f296ff3ed4e206faf0eab6b9ea
carlosalvarezfiuner <carlosalvarezluna@gmail.com>
2025-03-12 12:05:10 -0300

    funciones agregadas

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+def funcion():
+    print("carlos")
+def funcion2():
+    print("3")
+        
